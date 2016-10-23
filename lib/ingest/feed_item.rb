29921bcd8acf225dc8afba6c9c550f9209a42007
Dan Benjamin <dan@benjamin.org>
2016-10-23 11:28:05 -0500

    Renaming content_encoded to content, which is standard, and bumping version

--- lib/ingest/feed_item.rb
+++ lib/ingest/feed_item.rb
@@ -1,10 +1,23 @@
 module Ingest
   class FeedItem
-    attr_accessor :author, :content, :description, :enclosure_length,
-                  :enclosure_type, :enclosure_url, :guid, :itunes_author,
-                  :itunes_block, :itunes_closed_captioned, :itunes_duration,
-                  :itunes_explicit, :itunes_image, :itunes_keywords,
-                  :itunes_subtitle, :itunes_summary, :link, :published_at,
+    attr_accessor :author,
+                  :content,
+                  :description,
+                  :enclosure_length,
+                  :enclosure_type,
+                  :enclosure_url,
+                  :guid,
+                  :itunes_author,
+                  :itunes_block,
+                  :itunes_closed_captioned,
+                  :itunes_duration,
+                  :itunes_explicit,
+                  :itunes_image,
+                  :itunes_keywords,
+                  :itunes_subtitle,
+                  :itunes_summary,
+                  :link,
+                  :published_at,
                   :title
   end
 end
